a3dc3cae34ac7c8dea383da48a5b0dabeec41729
Guilherme <guilhermem81@gmail.com>
2019-12-03 21:24:02 +0000

    Add files via upload

---
+++ src/projetopadraocomposto/SimplesDois.java
@@ -0,0 +1,9 @@
+package projetopadraocomposto;
+
+public class SimplesDois extends Componente{
+//======================================================================================================
+    public void informacao(){
+        System.out.println("Tipo: " + getNome() + "\nPreço: " + getPreco() + "€\n");
+    }
+//======================================================================================================
+}
